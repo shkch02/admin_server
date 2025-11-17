33a0f0501cf6b14f0a9ca6e6ea6bc5ad7a9c9379
shkch02 <cba7215@gmail.com>
2025-11-17 15:03:40 +0900

    버전정보 표시

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
       <div className="container">
         <div className="header">
           <h1>IPS 관리자 콘솔</h1>
-          <p>eBPF 시스템콜 기반 보안 위협 탐지, CICD테스트 5.3</p>
+          <p>eBPF 시스템콜 기반 보안 위협 탐지</p>
           <Navigation />
         </div>
         <Routes>
@@ -47,6 +47,7 @@ function App() {
           <Route path="/syscalls" element={<Syscalls />} />
           <Route path="/test-attack" element={<TestAttack />} />
         </Routes>
+        <p className="version-info">버전 (v1.1.1)</p>
       </div>
     </Router>
   )
